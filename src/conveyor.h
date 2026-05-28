4ff626f560d221dc58836ca8a4d86cd70ec975b4
leedohyun0929 <leedohyun0929@gm.gist.ac.kr>
2026-05-28 13:17:21 +0900

    conveyor.h

---
+++ src/conveyor.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "SimulationObject.h"
+#include "Products.h"
+#include <queue>
+#include <memory>
+
+class Conveyor : public SimulationObject {
+private:
+    std::string id;
+
+    std::queue<std::shared_ptr<Product>> items;
+
+    std::shared_ptr<SimulationObject> nextNode;
+
+    int delay;
+    int currentTick;
+
+public:
+    Conveyor(std::string id, int delay);
+
+    void setNextNode(std::shared_ptr<SimulationObject> next);
+
+    void pushItem(std::shared_ptr<Product> item) override;
+
+    void update(int tick) override;
+
+    std::string getInfo() const override;
+};
