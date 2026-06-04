dd3d06160cc6bd84d494ebca1a8086bd7ea139b9
leedohyun0929 <leedohyun0929@gm.gist.ac.kr>
2026-06-04 13:30:06 +0900

    Create EventLogPanel.h

---
+++ src/EventLogPanel.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "UIPanel.h"
+
+class EventLogPanel : public UIPanel {
+public:
+    void render(
+        const FactorySnapshot& snap,
+        SimulationCommand& cmd
+    ) override;
+};
