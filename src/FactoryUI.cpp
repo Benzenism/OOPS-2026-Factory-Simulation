b6ab0ffc76da4c20effb4a512a4e95bd58dfce65
leedohyun0929 <leedohyun0929@gm.gist.ac.kr>
2026-06-04 13:35:10 +0900

    Update FactoryUI.cpp

--- src/FactoryUI.cpp
+++ src/FactoryUI.cpp
@@ -142,22 +142,8 @@ void FactoryUI::render(const FactorySnapshot& snap) {
 
     ImGui::NextColumn();
 
-    ImGui::BeginChild("EventLogPanel", ImVec2(0, 0), true);
-    ImGui::Text("EVENT LOG");
-    ImGui::SameLine(ImGui::GetWindowWidth() - 70);
-    if (ImGui::Button("Clear")) currentCmd.clearLog = true;
-    ImGui::Separator();
+    logPanel.render(snap, currentCmd);
     
-    ImGui::BeginChild("LogScrollRegion", ImVec2(0, 0), false, ImGuiWindowFlags_HorizontalScrollbar);
-    for (auto it = snap.eventLogs.rbegin(); it != snap.eventLogs.rend(); ++it) {
-        ImVec4 color = ImVec4(0.8f, 0.8f, 0.8f, 1.0f);
-        if (it->find("[WARN]") != std::string::npos) color = ImVec4(1.0f, 0.4f, 0.4f, 1.0f);
-        else if (it->find("[INFO]") != std::string::npos) color = ImVec4(0.4f, 0.8f, 1.0f, 1.0f);
-        ImGui::TextColored(color, "%s", it->c_str());
-    }
-    ImGui::EndChild();
-    
-    ImGui::EndChild();
     ImGui::End();
 }
 
