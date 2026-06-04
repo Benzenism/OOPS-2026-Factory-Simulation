e531168e7b7dc4a2d20b621471f122177918b312
leedohyun0929 <leedohyun0929@gm.gist.ac.kr>
2026-06-04 13:32:17 +0900

    Update FactoryUI.h

--- src/FactoryUI.h
+++ src/FactoryUI.h
@@ -1,9 +1,11 @@
 #pragma once
 #include "SimulationTypes.h"
 #include <string>
+#include <EventLogPanel.h>
 
 class FactoryUI {
 private:
+    EventLogPanel logPanel;
     SimulationCommand currentCmd;
     std::string selectedMachineId = "";
 
